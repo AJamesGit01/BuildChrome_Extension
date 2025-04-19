d6538f9c97292443b1022a3c3fa681d53d8359b0
Allen James <allenjamesdatuin@gmail.com>
2025-04-19 16:44:23 +0800

    first commit

---
+++ main/index.js
@@ -0,0 +1,135 @@
+// Log out "Button clicked!" when the user clicks the "SAVE INPUT" button
+
+// let inputBtn = document.getElementById("input-btn")
+
+// inputBtn.addEventListener("click", function() {
+//     console.log("Button clicked from addEventListener")
+// })
+
+// Refactor the code so that it uses .addEventListener()
+// when you click the SAVE INPUT button
+
+// let inputBtn = document.getElementById("input-btn")
+
+// inputBtn.addEventListener("click", function() {
+//     console.log("Button clicked!")
+// })
+
+// Create two variables:
+// myLeads -> should be assigned to an empty array
+// inputEl -> should be assigned to the text input field
+
+// let myLeads = []
+// const inputEl = document.getElementById("input-el")
+// const inputBtn = document.getElementById("input-btn")
+
+// // Push the value "www.awesomelead.com" to myArray when the input button is clicked
+//     // instead of the hard-coded "www.awesomeleads.com" value
+//     // Google -> "get value from input field javascript"
+
+// inputBtn.addEventListener("click", function() {
+//     myLeads.push("www.awesomelead.com")
+//     console.log(myLeads)
+// })
+
+
+// let myLeads = [] 
+// const inputEl = document.getElementById("input-el")
+// const inputBtn = document.getElementById("input-btn")
+
+// inputBtn.addEventListener("click", function() {
+    // Push the value from the inputEl into the myLeads array
+    // const inputValue = inputEl.value // -----> The varible Get the value from the input field 
+    // instead of the hard-coded "www.awesomeleads.com" value
+    // Google -> "get value from input field javascript"
+    // myLeads.push(inputValue) //------> Push the value from the variable into the myLeads array
+    
+    // myLeads.push(inputEl.value) // Push the value from the inputEl into the myLeads array without the variable
+
+    // console.log(myLeads) // Log the myLeads array to the console
+// })
+
+// Use for loop to log out all the items in the myLeads array
+
+// let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+// const inputEl = document.getElementById("input-el")
+// const inputBtn = document.getElementById("input-btn")
+// 2. Grab the unordered list and store it in a const variable called ulEl
+// const ulEl = document.getElementById("ul-el")
+
+// console.log(ulEl) // Log the ulEl to the console
+
+// inputBtn.addEventListener("click", function() {
+//     myLeads.push(inputEl.value)
+//     console.log(myLeads) 
+// }) // Log out the items in the myLeads array using a for loop 
+
+// Render the leads in the unordered list using ulEl.textContent
+
+// for (let i = 0; i < myLeads.length; i++) {
+    //  ulEl.textContent += myLeads[i] + " "  // Add the lead to the unordered list
+
+    // Replace .textContent with .innerHTML and use <li> tags
+    // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>" // Add the lead to the unordered list using innerHTML
+
+    // console.log(myLeads[i]) // Log out the items in the myLeads array using a for loop
+
+        // create element
+    // set text content
+    // append to ul
+//     const li = document.createElement("li")
+//     li.textContent = myLeads[i]
+//     ulEl.append(li)
+// } 
+
+//-------------------------------------------------------------------------------//
+
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+
+    // Clear out the input field
+    inputEl.value = ""
+
+    // 2. Call the renderLeads() function
+    renderLeads()
+
+    // console.log(myLeads)
+})
+
+// 1. Create a variable, listItems, to hold all the HTML for the list items
+// Assign it to an empty string to begin with
+
+// 1. Wrap the code below in a renderLeads() function
+
+function renderLeads() {
+
+    let listItems = ""
+
+    for (let i = 0; i < myLeads.length; i++) {
+
+        // Wrap the lead in an anchor tag (<a>) inside the <li>
+        // Can you make the link open in a new tab?
+
+        // listItems += "<li><a target='_blank' href='https://www.google.com/" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+        listItems += `
+        <li>
+            <a target='_blank' href='${myLeads[i]}'>
+                ${myLeads[i]}
+            </a>
+        </li>
+    `
+
+        // Add the item to the listItems variable instead of the ulEl.innerHTML
+        // listItems += "<li>" + myLeads[i] + "</li>"
+    }
+        // Render the listItems inside the unordered list using ulEl.innerHTML
+    
+    ulEl.innerHTML = listItems // Render the listItems inside the unordered list using ulEl.innerHTML   
+ 
+}
+
